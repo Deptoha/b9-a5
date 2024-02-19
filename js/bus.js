fdcb31615f69a10337c92654896c8f32b7ba7172
Deptoha <tohakhan249156@gmail.com>
2024-02-19 17:03:08 +0600

    added new js

---
+++ js/bus.js
@@ -0,0 +1,44 @@
+function A1(){
+    const removeHidden = document.getElementById('row-1')
+    removeHidden.classList.remove('hidden')
+
+    // alert('')
+}
+
+function A2(){
+    const removeHidden = document.getElementById('row-2')
+    removeHidden.classList.remove('hidden')
+
+}
+
+
+function A3(){
+    const removeHidden = document.getElementById('row-3')
+    removeHidden.classList.remove('hidden')
+}
+
+function A4(){
+    const removeHidden = document.getElementById('row-4')
+    removeHidden.classList.remove('hidden')
+}
+
+function B1(){
+    const removeHidden = document.getElementById('row-5')
+    removeHidden.classList.remove('hidden')
+}
+
+function B2(){
+    const removeHidden = document.getElementById('row-6')
+    removeHidden.classList.remove('hidden')
+}
+
+
+function B3(){
+    const removeHidden = document.getElementById('row-7')
+    removeHidden.classList.remove('hidden')
+}
+
+function B4(){
+    const removeHidden = document.getElementById('row-8')
+    removeHidden.classList.remove('hidden')
+}
--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,4 @@ const btnScroll = document.getElementById('scroll');
 btnScroll.addEventListener("click",function(){
     window.scrollTo(0, 1250);
 })
+
